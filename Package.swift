16e7133d8aada93314c020406233ca0958dd7652
Wolfgang Lutz <wlut@num42.de>
2025-04-22 22:56:30 +0200

    add executable product

--- Package.swift
+++ Package.swift
@@ -6,6 +6,9 @@ import PackageDescription
 let package = Package(
     name: "SwiftThumbnailer",
     platforms: [.macOS(.v12)],
+	products: [
+    	.executable(name: "SwiftThumbnailer", targets: ["SwiftThumbnailer"])
+	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
